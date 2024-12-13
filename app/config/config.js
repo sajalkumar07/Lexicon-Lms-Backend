7c3441dc3ade2c9c68c9f9ea572af5dc0b046ac0
Sajal <sajalkumarr030@gmail.com>
2024-12-13 15:39:33 +0530

    endpoint changed

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,9 @@
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://lms-studium.up.railway.app"],
+  origin: [
+    "http://localhost:3000",
+    "https://lms-studium.up.railway.app",
+    "https://lexicon-lms-backend.onrender.com",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
